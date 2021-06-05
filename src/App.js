1b1c56990fe21316dd272c50307517a1f2cac432
andy silva <andssilva.23@gmail.com>
2021-06-04 22:15:02 -0400

    feat: :rocket: Create a simple form in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import data from './data'
+import DadosPessoais from './components/DadosPessoais'
+import UltimoTrabalho from './components/UltimoTrabalho';
+import DadosGerais from './components/DadosGerais';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const INITIAL_STATE = {
+  nome: '',
+  email: '',
+  cpf: '',
+  adress: '',
+  city: '',
+  state: '',
+  moradia: '',
+  cv: '',
+  job: '',
+  description: '',
+  submitted: false,
+}
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = INITIAL_STATE;
+  }
+
+  handleChanges = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      moradia: event.target.value
+    })
+  }
+
+  handleBlur = (event) => {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    numbers.forEach((number) => {
+      if (event.target.value.startsWith(number)) {
+        event.target.value = '';
+      }
+    })
+  }
+
+  resetForm = () => {
+    this.setState(INITIAL_STATE)
+  };
+
+  renderSubmit = (event) => {
+    event.preventDefault();
+    this.setState({submitted: true});
+  }
+
+  render() {
+    const { nome, email, cpf, adress, city, state, cv, job, description, submitted } = this.state;
+
+    return (
+      <div>
+        <h1>Cadastro de Currículo</h1>
+        <form>
+          <DadosPessoais
+            onChange={this.handleChanges}
+            onClick={this.handleClick}
+            onBlur={this.handleBlur}
+            value={{ nome, email, cpf, adress, city, state, data }} />
+
+          <UltimoTrabalho
+            onChange={this.handleChanges}
+            value={{ cv, job, description }} />
+        </form>
+        <button className="submit" onClick={this.renderSubmit}>Enviar</button>
+        <button className="clear" onClick={this.resetForm}>Limpar</button>
+        <div className="renderInfo">
+          { submitted && <DadosGerais 
+          currentStates={this.state}/> }
+        </div>
+      </div >
+    )
+  }
 }
 
 export default App;
