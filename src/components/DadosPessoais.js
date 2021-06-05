5be82606ef5f8c1a2aa21829092ecd44f579f704
andy silva <andssilva.23@gmail.com>
2021-06-04 22:30:38 -0400

    fix: :bug: Fixes README file

--- src/components/DadosPessoais.js
+++ src/components/DadosPessoais.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class DadosPessoais extends Component {
   render() {
-    // const brazilStates = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
 
     const { value: { nome, email, cpf, adress, city, state, data }, onChange, onClick, onBlur } = this.props;
 
